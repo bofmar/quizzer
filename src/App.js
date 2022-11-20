4b88c043a0a6cdfad667ee9f3193b39b8811e059
bofmar <bofmar@outlook.com>
2022-11-20 16:23:15 +0200

    Add decoration elements

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App center--content">
+      <div className='decoration decoration--left'></div>
+      <div className='decoration decoration--right'></div>
       <IntroScreen />
     </div>
   );
