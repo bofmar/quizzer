80bc5b9086f8ab77f99b956f1fb56f34326f98d8
bofmar <bofmar@outlook.com>
2022-11-20 16:39:38 +0200

    Query opentdb for the questions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import IntroScreen from './components/IntoScreen';
 import './App.css';
 
 function App() {
   const [startGame, setStartGame] = useState(false);
+  const [questions, setQuestions] = useState([]);
 
   function startQuiz() {
     setStartGame(true);
   }
+
+  useEffect(() => {
+    (async function getQuiz() {
+      const response = await fetch('https://opentdb.com/api.php?amount=5');
+      const data = await response.json();
+      setQuestions(data.results);
+      console.log(questions);
+    })();
+
+  }, [startGame]);
 
   return (
     <div className="App center--content">
