27f8dc4f2e5d50049f3cca1d2636be7456fb858b
bofmar <bofmar@outlook.com>
2022-11-21 13:25:16 +0200

    Display answers using the parent's state

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,22 +1,11 @@
 import React, { useState } from "react";
 
 export default function Question({ question, registerQuestion }) {
-  const [answers, setAnswers] = useState(shuffle([question.correct_answer, ...question.incorrect_answers]));
-
-  function shuffle(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
-
-  }
-
   return (
     <div>
       <h1 dangerouslySetInnerHTML={{ __html: question.question }}></h1>
       <div className='answers'>
-        {answers.map(answer => <button className='question--button' dangerouslySetInnerHTML={{ __html: answer }} key={answer} onClick={(event) => registerQuestion(event, question.id, answer)}></button>)}
+        {question.answers.map((answer, index) => <button className='question--button' dangerouslySetInnerHTML={{ __html: answer.answerText }} key={answer.answerText} ></button>)}
       </div>
     </div>
   )
